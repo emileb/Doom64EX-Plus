1b15fe0a6e399708abf8e539b272e757c2494c30
Emile Belanger <emile.belanger@gmail.com>
2025-05-24 22:40:34 +0100

    Fix for Linux

--- src/engine/mobile/Android_src.mk
+++ src/engine/mobile/Android_src.mk
@@ -16,7 +16,7 @@ LOCAL_C_INCLUDES :=     $(SDL_INCLUDE_PATHS)  \
                         $(TOP_DIR)/Clibs_OpenTouch/idtech1 \
                         $(LOCAL_PATH)/mobile  \
                         $(TOP_DIR)/gl4es/include \
-                        $(TOP_DIR)\AudioLibs_OpenTouch\fluidsynth\include\
+                        $(TOP_DIR)/AudioLibs_OpenTouch/fluidsynth/include\
 
 
 LOCAL_SRC_FILES =   ../../../../Clibs_OpenTouch/idtech1/android_jni.cpp \
